d4e6b9e44638e4413f9a18022aa3c5789789d44e
Denis <denis.crudu@rodeapps.com>
2016-05-15 19:21:03 +0300

    Finished typecheck for basic operations - Arithmetic, Logic and Comaparison

--- main.ml
+++ main.ml
@@ -20,8 +20,9 @@ let interp e env heap =
 
 (* A few test cases *)
 let assert_raises f x =
-  try ignore(f x); false with
-  | _ -> true
+  try ignore (f x); false with
+  | Failure _ -> true
+  | _ -> false
   
 let _ = assert (Prim (Int 22) = (interp (Prim (Int 22)) myEnv myHeap));;
 
@@ -64,6 +65,91 @@ let _ = assert ( getFirstVarValue ( popFirstVar ( popFirstVar ([
 								  	 ("myVar2", TypeVal (TPrimitive, (Int 6)));
 								  	 ("myVar3", TypeVal (TPrimitive, (Int 7)));
 								  ]))) = (Int 7) )
+
+let empty = []
+
+(************  Typecheck tests   *************)
+(* PRIM *)
+let _ = assert ( TInt = typecheck empty (Prim (Int 5)))
+let _ = assert ( TFloat = typecheck empty (Prim (Float 5.0)))
+let _ = assert ( TBool = typecheck empty (Prim (Bool true)))
+let _ = assert ( TUnit = typecheck empty (Prim Unit))
+
+(* ADD *)
+let _ = assert ( TInt = typecheck empty (Add(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TFloat = typecheck empty (Add(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (Add(Prim(Int 11), Prim(Float 11.0))))
+
+(* SUB *)
+let _ = assert ( TInt = typecheck empty (Sub(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TFloat = typecheck empty (Sub(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (Sub(Prim(Int 11), Prim(Float 11.0))))
+
+(* MULT *)
+let _ = assert ( TInt = typecheck empty (Mult(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TFloat = typecheck empty (Mult(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (Mult(Prim(Int 11), Prim(Float 11.0))))
+
+(* DIV *)
+let _ = assert ( TInt = typecheck empty (Div(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TFloat = typecheck empty (Div(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (Div(Prim(Int 11), Prim(Float 11.0))))
+
+(* AND *)
+let _ = assert ( TBool = typecheck empty (And(Prim(Bool true), Prim(Bool true))))
+let _ = assert (true = assert_raises (typecheck empty) (And(Prim(Bool true), Prim(Int 5))))
+
+(* OR *)
+let _ = assert ( TBool = typecheck empty (Or(Prim(Bool true), Prim(Bool true))))
+let _ = assert (true = assert_raises (typecheck empty) (Or(Prim(Bool true), Prim(Int 5))))
+
+(* NOT *)
+let _ = assert ( TBool = typecheck empty (Not(Prim(Bool true))))
+let _ = assert (true = assert_raises (typecheck empty) (Not(Prim(Int 5))) )
+
+
+(* Equals *)
+let _ = assert ( TBool = typecheck empty (Equals(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TBool = typecheck empty (Equals(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (Equals(Prim(Int 11), Prim(Float 11.0))))
+
+(* NotEquals *)
+let _ = assert ( TBool = typecheck empty (NotEquals(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TBool = typecheck empty (NotEquals(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (NotEquals(Prim(Int 11), Prim(Float 11.0))))
+
+(* Less *)
+let _ = assert ( TBool = typecheck empty (Less(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TBool = typecheck empty (Less(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (Less(Prim(Int 11), Prim(Float 11.0))))
+
+(* LessOrEquals *)
+let _ = assert ( TBool = typecheck empty (LessOrEquals(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TBool = typecheck empty (LessOrEquals(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (LessOrEquals(Prim(Int 11), Prim(Float 11.0))))
+
+(* Greater *)
+let _ = assert ( TBool = typecheck empty (Grater(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TBool = typecheck empty (Grater(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (Grater(Prim(Int 11), Prim(Float 11.0))))
+
+(* GreaterOrEquals *)
+let _ = assert ( TBool = typecheck empty (GraterOrEquals(Prim(Int 11), Prim(Int 11))))
+let _ = assert ( TBool = typecheck empty (GraterOrEquals(Prim(Float 11.0), Prim(Float 11.0))))
+
+let _ = assert (true = assert_raises (typecheck empty) (GraterOrEquals(Prim(Int 11), Prim(Float 11.0))))
+
+
+
 (*
 let _ = assert ( getFirstVarValue ("myVar",TPrimitive, (Int 5)) = 5 )
 let _ = assert ( getFirstVarType ("myVar",TPrimitive, (Int 5)) = TPrimitive )
--- typecheck.ml
+++ typecheck.ml
@@ -1,82 +1,6 @@
 open Ast;;
 open Interpreter;;
 
-(* 
-type tPrim = TInt | TFloat | TBool | TUnit
-
-type tVal = 
- | Unit (* ok *)
- | Int of int (* ok *)
- | Float of float (* ok *)
- | Bool of bool (* ok *)
- | Evoid (* ok *)
- | Loc of int (* ok *)
-
-type typ = 
-    | TPrimitive
-    | TClass
-    | TBot  
-
-(* --------- *)
-(*    Alex   *)
-(* --------- *)
-
-(*Environment vars*)
-type typVal = TypeVal of typ*tVal
-
-(* This should be a stack *)
-type varEnv = VarEnv of (string*typVal) list
-(*-------------------*)
-
-(*Heap vars*)
-type fieldEnv = FieldEnv of string*typVal list
-
-type objVal = ObjVal of string*fieldEnv 
-
-type heap = Heap of (int*objVal) list
-(*-------------------*)
-
-type expr =
-  | Prim of tVal 
-  | Var of string (* ok *)
-  | Field of string*string (**)
-  | AssignVar of string*expr (* ok *)
-  | AssignField of string*string*expr (**)
-  | Sequence of expr*expr 
-  | If of expr*expr*expr 
-  | Add of expr*expr (* ok *)
-  | Sub of expr*expr (* ok *)
-  | Mult of expr*expr (* ok *)
-  | Div of expr*expr  (* ok *)
-  | And of expr*expr (* ok *)
-  | Or of expr*expr (* ok *)
-  | Not of expr (* ok *)
-  | While of expr*expr 
-  | BlockWithVar of typ*string*expr (**)
-  | BlockWithoutVar of expr 
-  | Ret of string*tVal (* *)
-
-type fieldDecl = FieldDecl of typ*string
-
-type fieldDeclList = FieldDeclList of fieldDecl list
-
-type parameter = Parameter of typ*string
-
-type parameterList = ParameterList of parameter list
-
-type methodDecl = MethodDecl of typ*string*parameterList*expr
-
-type methodDeclList = MethodDeclList of methodDecl list
-
-type classDecl = ClassDecl of string*string*fieldDeclList*methodDeclList
-
-type prog = Prog of (string*classDecl) list
-*)
-(*type check *)
-
-(* The empty context. *)
-let empty = []
-
 (* [lookup c x] is the type of [x] according to context [c]. *)
 let lookup ctx x =
   try List.assoc x ctx with
@@ -90,7 +14,8 @@ let extend ctx x t =
 
 
 let rec typecheck ctx = function
-  | Var x -> lookup ctx x
+  | GetVal x -> lookup ctx x
+  (* | AssignVar(name, e1) -> typecheck_assign_var ctx name e1 *)
   | Prim n -> typecheck_prim n
   | Add(e1,e2) -> typecheck_add ctx e1 e2
   | Sub(e1, e2) -> typecheck_sub ctx e1 e2
@@ -98,14 +23,14 @@ let rec typecheck ctx = function
   | Div(e1, e2) -> typecheck_div ctx e1 e2
   | And(e1,e2) -> typecheck_and ctx e1 e2
   | Or(e1, e2) -> typecheck_or ctx e1 e2
+  | Equals(e1, e2) | NotEquals(e1, e2) | Less(e1, e2) | LessOrEquals(e1, e2) | GraterOrEquals(e1, e2) | Grater(e1, e2) -> typecheck_comparison ctx e1 e2
   | Not e1 -> typecheck_not ctx e1
   (* | If(e1,e2,e3) -> typecheck_if ctx e1 e2 e3 *)
-  (* | AssignVar(name, e1) -> typecheck_assign_var ctx name e1 *)
   | _ ->failwith "Type check error"
 
 and
 
-(*******  ADD  ********)
+(*******  Prim  ********)
   typecheck_prim n =
   match n with
   | Int _ -> TInt
@@ -175,6 +100,16 @@ and
   match typecheck ctx e1 with
     | TBool -> TBool
     | _ -> failwith "Type error (not)"
+
+
+and
+
+(*******  COMPARISON  ********)
+  typecheck_comparison ctx e1 e2 =
+  match (typecheck ctx e1, typecheck ctx e2) with
+    | (TInt, TInt) -> TBool
+    | (TFloat, TFloat) -> TBool
+    | _ -> failwith "Type error (Comparison)"
 
 (* and
 

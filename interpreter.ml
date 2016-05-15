73adbf4b5aa844dfbfba482423f1b097b9623b07
Kerezsi-Alex <kerezsi.alex@hotmail.com>
2016-05-15 14:29:32 +0300

    Pull request

--- interpreter.ml
+++ interpreter.ml
@@ -59,6 +59,23 @@ let getVal a stk =
  try List.assoc a stk with
   | Not_found -> failwith "Type error (unbound variable)"
 
+(*
+let isDefinedVar var = function
+ | []     -> false
+ | (h::t) -> match h with
+             | (tv,_) -> 
+                          if tv = var then true 
+                          else isDefinedVar var t
+             | _ -> 
+
+  if (isDefinedVar var V) && (loc = getVal var V) &&
+     (isLocation loc) && ( isDefinedHeap loc ) &&
+     (fldE = getFieldE loc H) && (isDefinedVar fld fldE) &&
+     (val = getVal fld fldE) then *)
+ 
+
+
+
 (* -- Environment var operations -- *)
 
 (* -- Heap operations -- *)
@@ -117,6 +134,12 @@ let rec step env heap = function
   | Div(e1,e2)      -> step_div e1 e2 env heap
   | Or(e1,e2)       -> step_or e1 e2 env heap
   | Not(e)          -> step_not e env heap
+  | Equals(e1,e2)   -> step_eql e1 e2 env heap
+  | NotEquals(e1,e2)-> step_neql e1 e2 env heap
+  | Less(e1,e2)     -> step_less e1 e2 env heap
+  | LessOrEquals(e1,e2) -> step_lessEql e1 e2 env heap
+  | Grater(e1,e2)   -> step_grater e1 e2 env heap
+  | GraterOrEquals(e1,e2) -> step_graterEql e1 e2 env heap
   | While(e1,e2)    -> step_while e1 e2 env heap
   | Sequence(e1,e2) -> step_seq e1 e2 env heap
   | BlockWithoutVar(e1) -> step_block_nvar e1 env heap
@@ -128,8 +151,9 @@ let rec step env heap = function
  
 
 and
-  step_assign v e env heap = e
-
+  step_assign v e env heap = Prim( Int 10)
+  
+    
 and  
  
  step_ret v e env heap =  Prim (Int 10)
@@ -223,6 +247,69 @@ and
     | _ -> failwith "Run-time type error: not"
   else Not(step env heap e)
 
+(*------CMP---------*)
+
+and
+  step_eql e1 e2 env heap =
+  if is_primitive_val e1
+  then if is_primitive_val e2
+    then match (e1,e2) with
+      | (Prim (Bool x),Prim (Bool y)) -> Prim(Bool (x = y))
+      | _ -> failwith "Run-time type error: and"
+    else Equals(e1, step env heap e2)
+  else Equals(step env heap e1, e2)
+
+and
+  step_neql e1 e2 env heap =
+  if is_primitive_val e1
+  then if is_primitive_val e2
+    then match (e1,e2) with
+      | (Prim (Bool x),Prim (Bool y)) -> Prim(Bool (x <> y))
+      | _ -> failwith "Run-time type error: and"
+    else NotEquals(e1, step env heap e2)
+  else NotEquals(step env heap e1, e2)
+
+  and
+  step_less e1 e2 env heap =
+  if is_primitive_val e1
+  then if is_primitive_val e2
+    then match (e1,e2) with
+      | (Prim (Bool x),Prim (Bool y)) -> Prim(Bool (x < y))
+      | _ -> failwith "Run-time type error: and"
+    else Less(e1, step env heap e2)
+  else Less(step env heap e1, e2)
+
+  and
+  step_lessEql e1 e2 env heap =
+  if is_primitive_val e1
+  then if is_primitive_val e2
+    then match (e1,e2) with
+      | (Prim (Bool x),Prim (Bool y)) -> Prim(Bool (x <= y))
+      | _ -> failwith "Run-time type error: and"
+    else Less(e1, step env heap e2)
+  else Less(step env heap e1, e2)
+
+  and
+  step_grater e1 e2 env heap =
+  if is_primitive_val e1
+  then if is_primitive_val e2
+    then match (e1,e2) with
+      | (Prim (Bool x),Prim (Bool y)) -> Prim(Bool (x > y))
+      | _ -> failwith "Run-time type error: and"
+    else Grater(e1, step env heap e2)
+  else Grater(step env heap e1, e2)
+
+  and
+  step_graterEql e1 e2 env heap =
+  if is_primitive_val e1
+  then if is_primitive_val e2
+    then match (e1,e2) with
+      | (Prim (Bool x),Prim (Bool y)) -> Prim(Bool (x >= y))
+      | _ -> failwith "Run-time type error: and"
+    else GraterOrEquals(e1, step env heap e2)
+  else GraterOrEquals(step env heap e1, e2)
+
+(*------CMP---------*)
 and
   step_while e1 e2 env heap =
   if is_primitive_val e1
